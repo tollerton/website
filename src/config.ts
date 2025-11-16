ca1577d15e97c7eec66f590e231792da117b0f04
tollerton <heytollerton@gmail.com>
2025-11-16 18:22:27 +0000

    Initial commit

--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,11 @@ import type { ThemeConfig } from './types'
 export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
-    website: 'https://astro-chiri.netlify.app/', // Site domain
-    title: 'CHIRI', // Site title
+    website: 'https://tollerton.design/', // Site domain
+    title: 'Shaun Tollerton', // Site title
     author: '3ASH', // Author name
-    description: 'Minimal blog built by Astro', // Site description
-    language: 'en-US' // Default language
+    description: 'Senior UX Designer at Google. Previously Deliveroo.', // Site description
+    language: 'en-UK' // Default language
   },
 
   // GENERAL SETTINGS ////////////////////////////////////////////////////////////////////////////////////
